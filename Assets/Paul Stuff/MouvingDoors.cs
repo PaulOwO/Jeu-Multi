ebdbcf682a5de6cb969d890d67e34f6930e10c57
chevre hurlante lol <paul.breuza@gmail.com>
2021-03-07 17:17:58 +0100

    mad done

---
+++ Assets/Paul Stuff/MouvingDoors.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouvingDoors : MonoBehaviour
+{
+
+  
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+        Debug.Log("Doors mouve");
+        //position += Vector3.up * 10.0f;
+        transform.Translate(0, -3, 0);
+        }
+    }
+}

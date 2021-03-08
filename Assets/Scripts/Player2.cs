1a3ef96e9965e4a1ee4c2bd102ea68b6ced3dfdd
FlorianRossignol <rossignol.florian.fr@gmail.com>
2021-03-08 10:51:58 +0100

    add script for player 2

--- Assets/Scripts/Player2.cs
+++ Assets/Scripts/Player2.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player1 : MonoBehaviour
+public class Player2 : MonoBehaviour
 {
     [SerializeField] Rigidbody2D Bodyplayer_;
     private const float MoveSpeed_ = 1.0f;
@@ -10,26 +10,26 @@ public class Player1 : MonoBehaviour
     private const float Jumpspeed_ = 2.0f;
     private const int Jumpcountmax_ = 1;
     private int Jumpcountcurrent_ = 0;
-    // Start is called before the first frame update
     void Start()
     {
         Bodyplayer_ = GetComponent<Rigidbody2D>();
     }
 
+    // Update is called once per frame
     private void Update()
     {
-        if (Input.GetButtonDown("JUMP"))
+        if (Input.GetButtonDown("JUMP2"))
         {
-            if (Jumpcountcurrent_ >0)
+            if (Jumpcountcurrent_ > 0)
             {
                 Jump();
             }
-        }    
+        }
     }
 
     private void FixedUpdate()
     {
-        Bodyplayer_.velocity = new Vector2(Input.GetAxis("Horizontal") * MoveSpeed_, Bodyplayer_.velocity.y);
+        Bodyplayer_.velocity = new Vector2(Input.GetAxis("Vertical") * MoveSpeed_, Bodyplayer_.velocity.y);
     }
 
     private void Jump()
